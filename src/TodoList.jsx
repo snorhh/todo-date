3eb5f96880391324b7aacb43c865fa2eccd24b87
Sanna <sanna.noronen@myy.haaga-helia.fi>
2024-09-30 20:14:47 +0300

    done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,68 +1,63 @@
-// Import useState from react
 import { useState } from "react";
 import './Header.css'
 import './App.css'
 import './Description.css'
+
 function TodoList() {
 
-// Declare states
-
-//tehdään olio datalle, joka lisätään todos-taulukkoon, kun nappia painetaan
+//info = object that is added to todos array, when add button clicked
 const [info, setInfo] =useState({desc:"", date: ""});
 const [todos, setTodos] = useState([]);
 
-//Muista, object srpeading pitää olla objectin sisällä, eli curly braces
+//Note to self, object spreading must be inside object = curly braces
 const handleDescription = (event) => {
-    setInfo({...info, desc: event.target.value});
+  setInfo({...info, desc: event.target.value});
 };
 
 const handleDate = (event) => {
-    setInfo({...info, date: event.target.value});
+  setInfo({...info, date: event.target.value});
 };
   
   
-  // Remember to call preventDefault() if using form
-  const addTodo = () => {
+const addTodo = () => {
 
-    if (!info.desc || !info.date){
-        alert("Write a description and date")
+  if (!info.desc || !info.date){
+      alert("Write a description and a date")
+  }
 
-    }
+  else {
+    //create a new todos array and add a shallow copy of info to it, so if info changes, it doesn't change in the array
+    setTodos([...todos, {...info}]);
+    setInfo({desc:"", date: ""});
+   }
 
-    else {
-        //tehdään infosta kopio-olio ja laitetaan se listalle, jotta siihen ei voi jälkikäteen tulla muutoksia
-        setTodos([...todos, {...info}]);
-        setInfo({desc:"", date: ""});
-   
-    }
+};
 
-  };
+  return(
 
-    return(
-
-        // fieldset & ledend: https://css-tricks.com/how-to-add-text-in-borders-using-basic-html-elements/
+        //fieldset & ledend from article: https://css-tricks.com/how-to-add-text-in-borders-using-basic-html-elements/
         <>
          <header className='header'>
         <h2>Simple TodoList</h2>
         </header>
         <fieldset>
             <legend>Add todo:</legend>
-        Description: <input onChange={handleDescription} value={info.desc} />
-        Date: <input onChange={handleDate} value={info.date} />
-        <button onClick={addTodo}>Add</button>   
+            Description: <input onChange={handleDescription} value={info.desc} />
+            Date: <input onChange={handleDate} value={info.date} />
+            <button onClick={addTodo}>Add</button>   
         </fieldset>
         <table className='App'>
-      <thead>
-        <th>Date</th><th>Description</th>
-      </thead>
-    <tbody>
-      {todos.map((todo, index) => (
-        <tr key={index}>
-          <td>{todo.date}</td><td>{todo.desc}</td>
-        </tr>
+          <thead>
+            <th>Date</th><th>Description</th>
+          </thead>
+          <tbody>
+              {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.date}</td><td>{todo.desc}</td>
+            </tr>
       ))}
-        </tbody>
-    </table>
+          </tbody>
+        </table>
 
       </>
 
@@ -71,4 +66,4 @@ const handleDate = (event) => {
 
 }
   
-  export default TodoList;
+export default TodoList;
